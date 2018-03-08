922a9bad3bb1ca9820733861b59a15ebd102fa41
SophiaBouchet <bouchet.sophia@gmail.com>
2018-03-08 07:44:39 +0100

    back + modifs interface

--- back/defis.php
+++ back/defis.php
@@ -4,7 +4,12 @@ if (is_ajax()) {
       if (isset($_POST["action"]) && !empty($_POST["action"])) { //Checks if action value exists
           $action = $_POST["action"];
           switch($action) { // Switch case for value of action
-              case "defis": get_defis(); break;
+              case "defis":
+                get_defis();
+                break;
+              case "categories":
+                get_categories();
+                break;
           }
       }
   }
@@ -15,33 +20,61 @@ if (is_ajax()) {
 }
 
 function get_defis(){
-    // renvoie tous les défis
+    // récupère tous les défis + toutes les catégories
     $return = $_POST;
     $defis = find('SELECT * FROM defis');
-    $nb_defis = count($defis);
+    $nbDefis = count($defis);
 
-    if($nb_defis == 0){
-        $return['defis'] = 0;
+    if($nbDefis == 0){
+        $result['defis'] = 0;
     }else{
-        $i = 0;
-        $result = array();
-        foreach($defis as $defi){
-            // on récupère tous les défis
-            $result["defi".$i]['id'] = $defi['id'];
-            $result["defi".$i]['titre'] = $defi['titre'];
-            $result["defi".$i]['nbPoints'] = $defi['nbPoints'];
-            $result["defi".$i]['description'] = $defi['description'];
-            $result["defi".$i]['descriptionSup'] = $defi['descriptionSup'];
-            $result["defi".$i]['cheminImage'] = $defi['cheminImage'];
-            $result["defi".$i]['niveau'] = $defi['niveau'];
-            $result["defi".$i]['idCategorie'] = $defi['idCategorie'];
-            $result["defi".$i]['idSponsor'] = $defi['idSponsor'];
-            $i++;
-            
-        }
+      // 1 on récupère tous les défis
+      $i = 0;
+      $result = array();
+      foreach($defis as $defi){
+          
+          $result["defi".$i]['id'] = $defi['id'];
+          $result["defi".$i]['titre'] = $defi['titre'];
+          $result["defi".$i]['nbPoints'] = $defi['nbPoints'];
+          $result["defi".$i]['description'] = $defi['description'];
+          $result["defi".$i]['descriptionSup'] = $defi['descriptionSup'];
+          $result["defi".$i]['cheminImage'] = $defi['cheminImage'];
+          $result["defi".$i]['niveau'] = $defi['niveau'];
+          $result["defi".$i]['idCategorie'] = $defi['idCategorie'];
+          $result["defi".$i]['idSponsor'] = $defi['idSponsor'];
+          $i++;
+      }
     }
     $result['json'] = json_encode($result);
     echo json_encode($result);
+}
+
+function get_categories(){
+  // récupère tous les défis + toutes les catégories
+  $return = $_POST;
+  $categories = find('SELECT * FROM categories');
+  $nbCategories = count($categories);
+
+  if($nbCategories == 0){
+      $result['categories'] = 0;
+  }else{
+    // 1 on récupère tous les défis
+    $i = 0;
+    $result = array();
+    if($nbCategories != 0){
+      // on récupère nos catégories
+      $i = 0;
+      foreach($categories as $categorie){
+        $result["categorie".$i]['id'] = $categorie['id'];
+        $result["categorie".$i]['nom'] = $categorie['nom'];
+        $result["categorie".$i]['cheminIcone'] = $categorie['cheminIcone'];
+        $result["categorie".$i]['couleur'] = $categorie['couleur'];
+        $i++;
+      }
+    }
+  }
+  $result['json'] = json_encode($result);
+  echo json_encode($result);
 }
 ?>
 
--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     var defis = []; // defis -- contient tous les défis sous forme d'un tableau à double entrée
-
+    var categories = []; // categories -- contient toutes les catégories sous forme d'un tableau à double entrée
     $('#amis-tab').on('click', function (e) {
         e.preventDefault();
         $(this).tab('show');
@@ -19,7 +19,28 @@ $(document).ready(function() {
     // Récupération des défis -- lors du clic sur le tab défis
     $('#menuContainer li.nav-item').on('click', '#defis-tab', function (e) {
         e.preventDefault();
-        
+
+        // on récupère d'abord les catégories (besoin pour les couleurs)
+        if(categories.length == 0){
+            // on n'a pas encore récupéré les catégories, on les récupère
+            var data = {
+                "action": "categories"
+            };
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: "back/defis.php", // Relative or absolute path to defis.php file
+                data: data,
+                success: function(data) {
+                    categories = JSON.parse(data['json']);
+                    if(categories['categories'] == 0){
+                        // pas de categories dans la bd
+                        return 0;
+                    }
+                }
+            });
+        }
+
         if(defis.length == 0){
             // on n'a pas encore récupéré les défis, on les récupère
             var data = {
@@ -28,40 +49,63 @@ $(document).ready(function() {
             $.ajax({
                 type: "POST",
                 dataType: "json",
-                url: "back/defis.php", // Relative or absolute path to response.php file
+                url: "back/defis.php", // Relative or absolute path to defis.php file
                 data: data,
                 success: function(data) {
                     defis = JSON.parse(data['json']);
 
                     // une fois récupérés, on affiche les titres des défis dans des boutons (tous lors de l'init de cet onglet)
-                    
                     var htmlToAdd = ""; // html a ajouter à nav
                     var strDefi; // chaine pour récupérer le défi
+                    var strImageDefi; // url de l'image du defi
+                    var strCategorie;
                     var i;
+                    var j;
+                    var couleurCategorie;
+
+                    // on itère sur la taille de defis car il n'y a que les defis dans cette variable
+                    if (defis['defis'] == 0){
+                        // pas de defis dans la bd
+                        return 0;
+                    }
+
                     for(i = 0; i < Object.keys(defis).length; i++){
                         strDefi = 'defi' + i;
-                        htmlToAdd += '<a class="btn btn-primary btn-defi" id="'+ strDefi +'" href="defi.html">'+ defis[strDefi]['titre'] + '</a>';
-                        // <a class="btn btn-primary btn-defi" href="defi.html">Défi 1</a>
+                        
+                        for(j = 0; j < Object.keys(categories).length; j++){
+                            // on récupère aussi la couleur de la catégorie du défi
+                            strCategorie = 'categorie' + j;
+                            if(defis[strDefi]['idCategorie'] == categories[strCategorie]['id']){
+                                couleurCategorie = categories[strCategorie]['couleur'];
+                                break;
+                           }
+                        }
+                        htmlToAdd += '<div class="col-xs-2 btn-image-defi" style ="background-color:' + couleurCategorie + ';"><a id="'+ strDefi +'" class="btn" href="defi.html"><img src="' + defis[strDefi]['cheminImage'] + '"><br>' + defis[strDefi]['titre'] + '</a></div>';
                     }
+                    // $('.container-btn-defi').innerHTML = htmlToAdd;
                     $('.container-btn-defi').html(htmlToAdd);
                 }
-            });   
+            });
         }
         $(this).tab('show');
     });
 
 
-    /*$('.btn-defi').on('click', function (e) {
+    $('.container-btn-defi').on('click', '.btn-image-defi', function (e) {
         // ici, je dois choper l'id du defi que je veux puis je peux afficher le défi dans une nouvelle page !
         // on ouvre defi.html avec les bonnes infos
         var strDefi = this.getAttribute('id'); // on récupère l'id du défi à ouvrir - donne defi0 pour le defi n°0
         // $();
+        // window.location = 'defi.html';
+        location.href = 'http://localhost/projet/defi.html';
+        alert(location.href);
+        /*window.location = nouvelleAdresse;
         window.open($(this).attr('href'));
         $('.template-defi h4').html(defis[strDefi]['titre']);
         $('.template-defi .zone1').html("<img src="' + defis[strDefi]['cheminImage']'">" + defis[strDefi]['description']);
         $('.template-defi .zone2').html(defis[strDefi]['descriptionSup']);
-        $('.template-defi .zone3').html('Nombre de points à gagner : <div class="nb-pts">' + defis[strDefi]['nbPoints'] + '</div><div>Je relève le défi !</div><div>Ajouter ce défi à ma wish-list !</div>');        
-    });*/
+        $('.template-defi .zone3').html('Nombre de points à gagner : <div class="nb-pts">' + defis[strDefi]['nbPoints'] + '</div><div>Je relève le défi !</div><div>Ajouter ce défi à ma wish-list !</div>');  */      
+    });
 
     $('#profil-tab').on('click', function (e) {
         e.preventDefault();
@@ -78,24 +122,21 @@ $(document).ready(function() {
     /* // A decommenter qd ok
 
     $('#menuContainer').html(`<ul class="nav nav-tabs" role="tablist">
+
             <li class="nav-item">
-                <a class="nav-link active" id="amis-tab" data-toggle="tab" href="#amis" role="tab" aria-controls="amis" aria-selected="true"><i class="fas fa-users"></i><br>Amis</a>
+                <a class="nav-link" id="wishlist-tab" data-toggle="tab" href="#wishlist" role="tab" aria-controls="wishlist" aria-selected="false"><i class="fas fa-list-ul"></i><br>Wishlist</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" id="wishlist-tab" data-toggle="tab" href="#wishlist" role="tab" aria-controls="wishlist" aria-selected="false"><i class="fas fa-clipboard-list"></i><br>Wishlist</a>
+                <a class="nav-link" id="actualite-tab" data-toggle="tab" href="#actualite" role="tab" aria-controls="actualite" aria-selected="false"><i class="fas fa-users"></i><br>Actu</a>
             </li>
 
             <li class="nav-item">
-                <a class="nav-link" id="actualite-tab" data-toggle="tab" href="#actualite" role="tab" aria-controls="actualite" aria-selected="false"><i class="fas fa-dot-circle"></i><br>Actu</a>
+                <a class="nav-link" id="defis-tab" data-toggle="tab" href="#defis" role="tab" aria-controls="defis" aria-selected="false"><i class="fas fa-flag-checkered"></i><br>Défis</a>
             </li>
-
+            
             <li class="nav-item">
-                <a class="nav-link" id="defis-tab" data-toggle="tab" href="#defis" role="tab" aria-controls="defis" aria-selected="false"><i class="fas fa-list-alt"></i> <br>Défis</a>
-            </li>
-
-            <li class="nav-item">
-                <a class="nav-link" id="profil-tab" data-toggle="tab" href="#profil" role="tab" aria-controls="profil" aria-selected="false"><i class="fas fa-user-circle"></i> <br>Profil</a>
+                <a class="nav-link" id="autres-tab" data-toggle="tab" href="#autres" role="tab" aria-controls="autres" aria-selected="false"><i class="fas fa-ellipsis-h"></i><br>Autres</a>
             </li>
         </ul>`
     );*/
